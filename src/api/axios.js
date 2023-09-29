743b01a7e7887fc75038b01f08eca5f6dc1f1de1
saragam443 <git@saragam.com.np>
2023-09-29 13:56:50 +0545

    update api address

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = "https://nodejs-auth-api-production.up.railway.app/api";
+// const BASE_URL = "https://nodejs-auth-api-production.up.railway.app/api";
+const BASE_URL = "https://nodejs-auth-api.onrender.com/api";
 
 export default axios.create({
   baseURL: BASE_URL,
