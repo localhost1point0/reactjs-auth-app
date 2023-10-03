a64cafed8d989e7e09788a3aa5000d9ddf8ddab5
saragam443 <git@saragam.com.np>
2023-10-03 19:17:47 +0545

    register defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import Register from "./components/Register";
+import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Layout from "./components/Layout";
