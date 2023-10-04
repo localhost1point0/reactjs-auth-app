1c781b727b45b8db0d7631628c346fb16be18731
saragam443 <git@saragam.com.np>
2023-10-04 10:30:47 +0545

    new api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://saragamsubedi.com.np/api";
+const BASE_URL = "https://saragamsubedi.com.np/auth";
 
 export default axios.create({
   baseURL: BASE_URL,
